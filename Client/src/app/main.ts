6c13155002e7f10221aafec4a332fe66fc001b10
Edanriell <Edanriell@gmail.com>
2024-10-14 12:34:43 +0300

    feature(input-validation-styles): added styles for states valid, invalid, idle

--- Client/src/app/main.ts
+++ Client/src/app/main.ts
@@ -2,12 +2,14 @@ import "./styles/styles.less";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { MotionPlugin } from "@vueuse/motion";
 
 import App from "./app.vue";
 import { router } from "./providers/vue-router";
 
 const app = createApp(App);
 
+app.use(MotionPlugin);
 app.use(createPinia());
 app.use(router);
 
