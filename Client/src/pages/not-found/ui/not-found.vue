ce48ef6de644d7d0c79cfa22b6fe036430a6c7b2
Edanriell <Edanriell@gmail.com>
2024-10-20 18:20:40 +0300

    feature(not-found-page): created not found page

--- Client/src/pages/not-found/ui/not-found.vue
+++ Client/src/pages/not-found/ui/not-found.vue
@@ -1,7 +1,51 @@
 <script lang="ts" setup></script>
 
 <template>
-	<h1>Not Found Page</h1>
+	<section class="not-found-section">
+		<h1 class="not-found-section__title">404</h1>
+		<p class="not-found-section__text">Sorry, the page you are looking for does not exist.</p>
+	</section>
 </template>
 
-<style scoped></style>
+<style scoped>
+	.not-found-section {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		row-gap: 20rem;
+		padding-right: 16rem;
+		padding-left: 16rem;
+
+		@media (width > 768px) {
+			padding-right: 40rem;
+			padding-left: 40rem;
+		}
+
+		@media (width > 1440px) {
+			padding-right: 160rem;
+			padding-left: 160rem;
+		}
+	}
+
+	.not-found-section__title {
+		font-family: var(--font-family), sans-serif;
+		font-weight: 500;
+		font-size: 64rem;
+		line-height: 90%;
+		letter-spacing: -0.05em;
+		color: #000;
+	}
+
+	.not-found-section__text {
+		font-family: var(--font-family), sans-serif;
+		font-weight: 200;
+		font-size: 28rem;
+		line-height: 90%;
+		letter-spacing: -0.04em;
+		color: #000;
+		text-align: center;
+		margin-bottom: 20rem;
+	}
+</style>
